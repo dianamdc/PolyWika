c181e9e2f449629cfc114b91d76c59aca1cbad8c
diana <diana@ULTIFLEX>
2019-10-31 12:50:44 +0800

    added homepage

---
+++ src/polywika/HomePageController.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package polywika;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import polywika.Tagalog.*;
+
+/**
+ * FXML Controller class
+ *
+ * @author diana
+ */
+public class HomePageController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @FXML
+    AnchorPane root;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }
+
+    public void handleButtonActionTagalog(ActionEvent e) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("Tagalog/TagalogFXML.fxml"));
+            Parent secondview = loader.load();
+            Scene newscene = new Scene(secondview);
+            TagalogFXMLController other = loader.getController();
+            Stage curstage = (Stage) root.getScene().getWindow();
+            curstage.setScene(newscene);
+        } catch (IOException ex) {
+            Logger.getLogger(HomePageController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+}
--- src/polywika/PolyWika.java
+++ src/polywika/PolyWika.java
@@ -19,7 +19,7 @@ public class PolyWika extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("HomePage.fxml"));
 
         Scene scene = new Scene(root);
 
