bd2f141b8a1ba89bc84679703616d9f1a6f863a1
diana <diana@ULTIFLEX>
2019-11-27 10:04:59 +0800

    added tag grammar

---
+++ src/polywika/Tagalog/TagalogGrammarController.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package polywika.Tagalog;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.awt.Button;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import polywika.Bisaya.BisayaFXMLController;
+import polywika.HomePageController;
+import polywika.Vocab;
+
+/**
+ * FXML Controller class
+ *
+ * @author Ribai
+ */
+public class TagalogGrammarController implements Initializable {
+
+    @FXML
+    AnchorPane root;
+    @FXML
+    Button HomePage;
+    @FXML
+    ChoiceBox Choices;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            String dir = "src/polywika/vocabfiles/tagaloggrammar.json";
+            Vocab v = objectMapper.readValue(new File(dir), Vocab.class);
+        } catch (IOException ex) {
+            Logger.getLogger(TagalogGrammarController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public void GoToHomeScreen(ActionEvent event) throws IOException {
+        {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/polywika/HomePageFXML.fxml"));
+            Parent secondview = loader.load();
+            Scene newscene = new Scene(secondview);
+            HomePageController other = loader.getController();
+            Stage curstage = (Stage) root.getScene().getWindow();
+            curstage.setScene(newscene);
+        }
+    }
+}
